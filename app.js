c97d4633b69cbfc99899d80236369297cdddddec
Eduardo José Silva dos Santos <eduardo.jose@santacasademaceio.com.br>
2021-12-14 17:58:54 -0300

    Criação do módulo de insert

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+//alter user 'root'@'localhost' identified with mysql_native_password by '124306'; 
+//flush privileges; 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
 const app = express();
-
+const urlEncodeParser=bodyParser.urlencoded({extended:false});
+const sql=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'124306',
+    port:'3306'
+});
+sql.query("use nodejs");
 
 //======================================
 //Template engine
@@ -24,17 +34,40 @@ var handlebarsConf = handlebars.create({
 app.engine('handlebars', handlebarsConf.engine);
 app.set('view engine', 'handlebars');
 
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+//app.use('/img', express.static('img'));
+
 
 //======================================
 //Routes and Templates
 //======================================
-app.get("/:id?", function(req, res){
+//app.get("/:id?", function(req, res){
+app.get("/", function(req, res){
     /*res.send("<h1>Página Inicial</h1>");*/
     /*res.sendFile(__dirname+"\\index.html");*/
 
     //console.log(req.params.id);
-    res.render('index', {id:req.params.id});
-    //res.render('index');
+    //res.render('index', {id:req.params.id});
+    res.render('index');
+});
+
+app.get("/javascript", function(req, res){
+    res.sendFile(__dirname+'/js/javascript.js');
+});
+
+app.get("/style", function(req, res){
+    res.sendFile(__dirname+'/css/style.css');
+});
+
+app.get("/inserir", function(req, res){
+    res.render("inserir");
+});
+
+app.post("/controllerForm", urlEncodeParser, function(req, res){
+    //console.log(req.body.name);
+    sql.query("insert into user values(?, ?, ?)", [req.body.id, req.body.name, req.body.age]);
+    res.render('controllerForm');
 });
 
 
